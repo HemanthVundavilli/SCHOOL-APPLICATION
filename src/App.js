8d8c1354c0db70f32845beb87a83999c727a1ff7
Hemanth Vundavilli <118917390+Hemu2503@users.noreply.github.com>
2025-04-12 23:10:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import Gallery from './components/pages/Gallery';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Academics from './components/pages/Academic';
+import FeesStructure from './components/pages/FeesStructure';
+import Admissions from './components/pages/Admissions';
+import Login from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/academics" element={<Academics />} />
+            <Route path="/fees-structure" element={<FeesStructure />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
